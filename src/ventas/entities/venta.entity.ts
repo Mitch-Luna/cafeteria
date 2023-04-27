ba44ec122e5c6ddc31967f07385148173b2f8762
Mitch-Luna <lunitamitch02@gmail.com>
2023-04-27 10:52:55 -0600

    modificaicones

--- src/ventas/entities/venta.entity.ts
+++ src/ventas/entities/venta.entity.ts
@@ -1,28 +1,29 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import { ClienteProducto } from "./producto-image.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { ClienteProducto } from './producto-image.entity';
 
 @Entity()
 export class Venta {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column({ type: 'text' })
-    nombreCliente: string;
-  
-    @Column({ type: 'text' })
-    direccion: string;
-  
-    @Column({ type: 'text' })
-    pais: string;
-  
-    @Column({ type: 'text' })
-    telefono: string;
-  
-    @Column({ type: 'text' })
-    fecha: string;
-    
-    @OneToMany(() => ClienteProducto, (clienProducto) => clienProducto.venta,{
+  @Column({ type: 'text' })
+  nombreCliente: string;
+
+  @Column({ type: 'text' })
+  direccion: string;
+
+  @Column({ type: 'text' })
+  pais: string;
+
+  @Column({ type: 'text' })
+  telefono: string;
+
+  @Column({ type: 'text' })
+  fecha: string;
+
+  @OneToMany(
+    () => ClienteProducto, (clienProducto) => clienProducto.venta, {
         cascade: true
-    })
-    images?: ClienteProducto[]
+  })
+  images?: ClienteProducto[];
 }
